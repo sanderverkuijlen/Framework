1770a5aa4fb7b82936b5fb8c889eef0b729a9103
Sander Verkuijlen <sanderverkuijlen86@gmail.com>
2012-09-12 22:20:30 +0200

    - Config update with database-related defines;

--- lib/config.php
+++ lib/config.php
@@ -1,4 +1,6 @@
 <?php
     define('FILE_ROOT',     'D:/Programs/xampp/htdocs/Password/');
     define('URL_ROOT',      'http://localhost/Password/');
+
+    require_once(FILE_ROOT.'lib/db_config.php');
 ?>
---
+++ lib/db_config.php
@@ -0,0 +1,9 @@
+<?php
+    //Database config
+    define('DB_HOSTNAME',       '');
+    define('DB_NAME',           '');
+    define('DB_USER',           '');
+    define('DB_PASSWORD',       '');
+
+    define('DB_ENCRYPTION_KEY', '');
+?>
